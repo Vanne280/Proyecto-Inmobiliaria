73d10c2e1c3825f2e4867d0fde74abe6c9c33e63
Vanessa Galvis <60371102+Vanne280@users.noreply.github.com>
2020-02-20 17:31:55 -0500

    Creación de navbar
    Se creó el navbar y herencia de plantillas con la página de ventas y arrendamiento

--- inmueble/views.py
+++ inmueble/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# funciones que llaman las páginas
+def home(request):
+    return render(request, 'base/base.html')
+
+def ventas(request):
+    return render(request, "paginas/ventas.html")
+
+def arrendamientos(request):
+    return render(request, "paginas/arrendamientos.html")
