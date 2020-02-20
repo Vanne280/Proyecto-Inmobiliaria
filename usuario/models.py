c3befd00fa324b688265995a38d043fb93c53bc3
Vanessa Galvis <60371102+Vanne280@users.noreply.github.com>
2020-02-19 22:00:39 -0500

    Vanne
    Cambios de nombre en los campos de las tablas ROL y PERFIL

--- usuario/models.py
+++ usuario/models.py
@@ -3,15 +3,15 @@ from django.contrib.auth.models import User
 
 #Se crea modelo de ROL
 class Rol(models.Model):
-    rol_nombre = models.CharField(max_length=30)
+    nombre = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.rol_nombre
+        return self.nombre
 
 #Se crea clase Perfil
 class Perfil(models.Model):
-    per_user = models.OneToOneField(User, on_delete = models.PROTECT)
-    per_rol = models.ForeignKey(Rol, on_delete = models.PROTECT)
+    IDUser = models.OneToOneField(User, on_delete = models.PROTECT)
+    IDRol = models.ForeignKey(Rol, on_delete = models.PROTECT)
 
     # def __str__(self):
     #     return self.per_user
